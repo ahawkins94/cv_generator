2b7e9858085b94941d2129d0516b7d7714930719
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-28 16:45:08 +0100

    copied in classwork to work off instead of flicking between terminals to test functionality

--- controllers/cv_controller.rb
+++ controllers/cv_controller.rb
@@ -1,5 +1,73 @@
 class CvController < Sinatra::Base
 
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+	set :root, File.join(File.dirname(__FILE__), "..")
+
+	set :views, Proc.new { File.join(root, 'views') }
+
+  $posts = [{
+      title: "Post 1",
+      body: "This is the first post"
+  },
+  {
+      title: "Post 2",
+      body: "This is the second post"
+  },
+  {
+      title: "Post 3",
+      body: "This is the third post"
+  }]
+
+	get '/posts' do
+		@page_header = "All the posts"
+		@posts = $posts
+		erb :"posts/index"
+	end
+
+	get '/posts/new' do
+		erb :"posts/new"
+	end
+
+	get '/posts/:id' do
+		id = params[:id].to_i
+		@page_header = "Post #{id+1}"
+		@post = $posts[id]
+		erb :"posts/shows"	
+	end
+
+	post "/posts" do
+		new_post = {
+			title: params[:title],
+			body: params[:body]
+		}
+		$posts << new_post
+		redirect '/posts'
+	end
+
+	get "/posts/:id/edit" do
+		@id = params[:id].to_i
+		@post = $posts[@id]
+
+		erb :"posts/edit"
+	end
+
+	put "/posts/:id" do
+		id = params[:id].to_i
+		$posts[id][:title] = params[:title]
+		$posts[id][:body] = params[:body]
+
+		redirect "/posts/#{id}"
+
+	end
+
+	delete '/posts/:id' do
+		id = params[:id].to_i
+		$posts.delete_at(id)
+		redirect '/posts'
+	end
 
 
 end
